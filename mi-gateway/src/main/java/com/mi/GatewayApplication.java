c93a1658053b797c078c88f31f1ba41f5ce1d07f
YangLi <woshi110>
2021-07-13 20:28:37 +0800

    项目初始化

---
+++ mi-gateway/src/main/java/com/mi/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.mi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName GatewayApplication
+ * @Author yangli
+ * @Date 2021/4/28 14:18
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
---
+++ mi-gateway/src/main/java/com/mi/fallback/ServerFallBack.java
@@ -0,0 +1,20 @@
+package com.mi.fallback;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName ServerFallBack
+ * @Author yangli
+ * @Date 2021/4/29 14:59
+ * @Description:
+ */
+@RestController
+public class ServerFallBack {
+
+    @RequestMapping("fallback/{name}")
+    public String serverFallback(@PathVariable("name") String name) {
+        return "微服务:" + name + "--访问超时,请稍后再试";
+    }
+}
